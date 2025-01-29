6126ce64560a107c5415ac41c290c5e9a9140c17
Marshall Scott <daarkzek@protonmail.com>
2025-01-29 22:59:42 +1300

    feat: Remove astro node

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -2,15 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
-import node from '@astrojs/node';
-
 import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), vue()],
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  integrations: [icon(), vue()]
 });
